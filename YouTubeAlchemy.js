89caf77831224bbb180a06e67cce0f01fd97ea69
Tim Macy <174056878+TimMacy@users.noreply.github.com>
2025-08-16 17:19:05 +0200

    Update YouTubeAlchemy.js
    adjustments for the latest YouTube changes (fixed playback speed display)

--- YouTubeAlchemy.js
+++ YouTubeAlchemy.js
@@ -3,7 +3,7 @@
 // @description  Toolkit for YouTube with 130+ options accessible via settings panels. Key features include: tab view, playback speed control, video quality selection, export transcripts, prevent autoplay, hide Shorts, disable play-on-hover, square design, auto-theater mode, number of videos per row, display remaining time adjusted for playback speed and SponsorBlock segments, persistent progress bar with chapter markers and SponsorBlock support, modify or hide various UI elements, and much more.
 // @author       Tim Macy
 // @license      AGPL-3.0-or-later
-// @version      7.12
+// @version      7.12.1
 // @namespace    TimMacy.YouTubeAlchemy
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @match        https://*.youtube.com/*
@@ -21,7 +21,7 @@
 *                                                                       *
 *                    Copyright © 2025 Tim Macy                          *
 *                    GNU Affero General Public License v3.0             *
-*                    Version: 7.12 - YouTube Alchemy                    *
+*                    Version: 7.12.1 - YouTube Alchemy                  *
 *                                                                       *
 *             Visit: https://github.com/TimMacy                         *
 *                                                                       *
@@ -6975,7 +6975,7 @@
         // display the speed
         const speedDisplay = document.createElement("span");
         speedDisplay.id = "CentAnni-speed-display";
-        speedDisplay.classList.add("CentAnni-playback-speed-display", "animated-rolling-number-wiz");
+        speedDisplay.classList.add("CentAnni-playback-speed-display", "animated-rolling-number-wiz", "animatedRollingNumberHost");
         speedDisplay.textContent = `${video.playbackRate}x`;
 
         // create minus and plus buttons
