411c40636f596b86a2201be7bdb79226aea2f2e5
Tim Macy <174056878+TimMacy@users.noreply.github.com>
2025-09-01 10:38:34 +0200

    Update YouTubeAlchemy.js

--- YouTubeAlchemy.js
+++ YouTubeAlchemy.js
@@ -3,7 +3,7 @@
 // @description  Toolkit for YouTube with 130+ options accessible via settings panels. Key features include: tab view, playback speed control, video quality selection, export transcripts, prevent autoplay, hide Shorts, disable play-on-hover, square design, auto-theater mode, number of videos per row, display remaining time adjusted for playback speed and SponsorBlock segments, persistent progress bar with chapter markers and SponsorBlock support, modify or hide various UI elements, and much more.
 // @author       Tim Macy
 // @license      AGPL-3.0-or-later
-// @version      7.17
+// @version      7.17.1
 // @namespace    TimMacy.YouTubeAlchemy
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @match        https://*.youtube.com/*
@@ -21,7 +21,7 @@
 *                                                                       *
 *                    Copyright © 2025 Tim Macy                          *
 *                    GNU Affero General Public License v3.0             *
-*                    Version: 7.17 - YouTube Alchemy                    *
+*                    Version: 7.17.1 - YouTube Alchemy                  *
 *                                                                       *
 *             Visit: https://github.com/TimMacy                         *
 *                                                                       *
@@ -1459,9 +1459,11 @@
 
         /* video tab view css */
         .CentAnni-video-tabView {
+            #secondary { min-height: 0; }
             .CentAnni-tabView {
                 position: relative;
                 display: flex;
+                min-height: 0;
                 flex-direction: column;
                 width: var(--ytd-watch-flexy-sidebar-width);
                 min-width: var(--ytd-watch-flexy-sidebar-min-width);
@@ -1540,15 +1542,13 @@
             }
 
             .CentAnni-tabView-content {
+                min-height: 0;
                 overflow-y: auto;
                 overflow-x: hidden;
                 padding: 0;
                 margin: 0;
-                max-height: calc(100dvh - var(--ytd-masthead-height, var(--ytd-toolbar-height)) - 2 * var(--ytd-margin-6x) - 52px) !important;
-            }
-
-            .CentAnni-tabView-content {
                 display: none;
+                max-height: calc(100dvh - var(--ytd-masthead-height, var(--ytd-toolbar-height)) - 2 * var(--ytd-margin-6x) - 52px) !important;
             }
 
             .CentAnni-tabView-content.active {
